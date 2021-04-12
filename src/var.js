eae6ff4805169641f5d0b5c9154491b35c342e32
Lev <levdanneman@gmail.com>
2021-04-12 10:40:18 +0300

    update

--- src/var.js
+++ src/var.js
@@ -1,4 +1,4 @@
-const BACK_PORT = "https://eyeknow-backend.herokuapp.com/api";
-// const BACK_PORT = `http://localhost:5000/api`;
+// const BACK_PORT = "https://eyeknow-backend.herokuapp.com/api";
+const BACK_PORT = `http://localhost:5000/api`;
 
 export { BACK_PORT };
